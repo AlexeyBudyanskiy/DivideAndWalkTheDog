92c7c77756b88d4ec5c74543e2628527e42ca05e
Alexey Budyanskiy <alexey-budyanskiy@yandex.ru>
2016-11-14 22:52:20 +0200

    Switch

--- XamarinTest/SecApp/SecApp/MiBandSDK/obj/Debug/generated/src/__NamespaceMapping__.cs
+++
@@ -1,4 +0,0 @@
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.jellygom.miband_sdk", Managed="Com.Jellygom.Miband_sdk")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.jellygom.miband_sdk.MiBandIO", Managed="Com.Jellygom.Miband_sdk.MiBandIO")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.jellygom.miband_sdk.MiBandIO.Listener", Managed="Com.Jellygom.Miband_sdk.MiBandIO.Listener")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.jellygom.miband_sdk.MiBandIO.Model", Managed="Com.Jellygom.Miband_sdk.MiBandIO.Model")]
